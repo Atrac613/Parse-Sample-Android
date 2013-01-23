c4c25b6c4a502e5d81d7259f10766c3638da8811
Osamu Noguchi <me@atrac613.io>
2013-01-23 20:57:03 +0900

    first commit.

---
+++ src/io/atrac613/parse/sample/MainActivity.java
@@ -0,0 +1,27 @@
+package io.atrac613.parse.sample;
+
+import com.parse.PushService;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        PushService.subscribe(this, "", MainActivity.class);
+        PushService.setDefaultPushCallback(this, MainActivity.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
